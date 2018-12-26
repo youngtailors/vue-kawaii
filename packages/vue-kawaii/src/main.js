9e28c9fd29138bd780781b2a1af53c005d41798f
Evan N <dacsang97@gmail.com>
2018-12-26 21:25:33 +0700

    feat - export ice cream, mug, planet, speech bubble

--- packages/vue-kawaii/src/main.js
+++ packages/vue-kawaii/src/main.js
@@ -2,5 +2,18 @@ import Backpack from './components/backpack/Backpack.vue'
 import Browser from './components/browser/Browser.vue'
 import CreditCard from './components/creditCard/CreditCard.vue'
 import Ghost from './components/ghost/Ghost.vue'
+import IceCream from './components/iceCream/IceCream.vue'
+import Mug from './components/mug/Mug.vue'
+import Planet from './components/planet/Planet'
+import SpeechBubble from './components/speechBubble/SpeechBubble'
 
-export { Backpack, Browser, CreditCard, Ghost }
+export {
+  Backpack,
+  Browser,
+  CreditCard,
+  Ghost,
+  IceCream,
+  Mug,
+  Planet,
+  SpeechBubble,
+}
